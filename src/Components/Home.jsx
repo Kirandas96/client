16f47290b3b05a357f6adf79229a022301e20356
Kirandas96 <parvathikiran10@gmail.com>
2023-02-01 14:14:38 +0530

    done

---
+++ src/Components/Home.jsx
@@ -0,0 +1,79 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+export const Home = () => {
+    const[employee,setEmployee]=useState([])
+    const[a,setA]=useState(false)
+
+    const getData=()=>{
+        axios.get("http://localhost:5000/data/employees")
+        .then((res)=>{
+          console.log(res);
+          setEmployee(res.data)
+        })
+    }
+    const handleClick=()=>{
+        setA(true)
+        getData()
+    }
+  return (
+    <div>Home
+        <div>
+            <button onClick={()=>handleClick()}>Details</button>
+        </div>
+        <div>
+            {a?<div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            no
+                        </th>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Email
+                        </th>
+                        <th>
+                            Age
+                        </th>
+                        <th>
+                            Position
+                        </th>
+                        <th>
+                            Salary
+                        </th>
+                    
+                    </tr>
+                </thead>
+                <tbody>
+{employee.map((el,index)=>{return(
+    <tr key={index}>
+        <th>
+            {el.id}
+        </th>
+        <th>
+                            {el.name}
+                        </th>
+                        <th>
+                        {el.email}
+                        </th>
+                        <th>
+                        {el.age}
+                        </th>
+                        <th>
+                        {el.position}
+                        </th>
+                        <th>
+                        {el.salary}
+                        </th>
+    </tr>
+)})}
+</tbody>
+</table>
+        </div>:<></>}
+        </div>
+    </div>
+  )
+}
